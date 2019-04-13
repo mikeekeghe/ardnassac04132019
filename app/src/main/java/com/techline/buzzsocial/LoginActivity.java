a9e09d8ca7e63628acdfa955a66c3c1fed59cacc
techlinesolutions <css.nigeri@gmail.com>
2019-04-13 01:07:58 +0100

    first commit

---
+++ app/src/main/java/com/techline/buzzsocial/LoginActivity.java
@@ -0,0 +1,70 @@
+package com.techline.buzzsocial;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import dev.niekirk.com.instagram4android.Instagram4Android;
+
+public class LoginActivity extends AppCompatActivity {
+    public static final String MyPREFERENCES = "MyPrefs";
+
+    SharedPreferences SP;
+    EditText pswd,usrusr;
+    TextView sup,lin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        lin = findViewById(R.id.lin);
+        usrusr = findViewById(R.id.usrusr);
+        pswd = findViewById(R.id.pswrdd);
+        sup = findViewById(R.id.sup);
+        Typeface custom_font = Typeface.createFromAsset(getAssets(), "fonts/LatoLight.ttf");
+        Typeface custom_font1 = Typeface.createFromAsset(getAssets(), "fonts/LatoRegular.ttf");
+        lin.setTypeface(custom_font1);
+        sup.setTypeface(custom_font);
+        usrusr.setTypeface(custom_font);
+        pswd.setTypeface(custom_font);
+
+
+
+
+        sup.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent it = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(it);
+                finish();
+            }
+        });
+
+    }
+
+    public void onClickButon(View view)  {
+        Instagram4Android instagram = Instagram4Android.builder().username(usrusr.getText().toString()).password(pswd.getText().toString()).build();
+        instagram.setup();
+        try {
+            instagram.login();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+/*        Instagram4Android instagram = new Instagram4Android(usrusr.getText().toString(),pswd.getText().toString());
+        instagram.setup();
+        try {
+            instagram.login();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+    }}
