ce7f9c51ea4197ba08b4c49e68eb33e743fe892e
techlinesolutions <css.nigeri@gmail.com>
2019-04-14 00:36:10 +0100

    third commit

--- app/src/main/java/com/techline/buzzsocial/HomeActivity.java
+++ app/src/main/java/com/techline/buzzsocial/HomeActivity.java
@@ -4,11 +4,24 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import dev.niekirk.com.instagram4android.requests.InstagramGetUserFollowersRequest;
+import dev.niekirk.com.instagram4android.requests.payload.InstagramGetUserFollowersResult;
+import dev.niekirk.com.instagram4android.requests.payload.InstagramUserSummary;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private static final String TAG = "HomeActivity";
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -30,6 +43,14 @@ public class HomeActivity extends AppCompatActivity {
             return false;
         }
     };
+    private Bundle extras;
+    private String full_name="",profile_pic_url=""
+    ,usernameInsta="",pk="",profile_pic_id="";
+    TextView tvDisplay_name, tvUserName, tvFolCount;
+    ImageView ivThumb;
+    ArrayList<String> followersUserNameList = new ArrayList<String>();
+    ArrayList<String> followersPicUrlList = new ArrayList<String>();
+    private String no_of_followers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +60,48 @@ public class HomeActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        extras = getIntent().getExtras();
+        if (extras != null) {
+
+            full_name = extras.getString("full_name");
+            profile_pic_url = extras.getString("profile_pic_url");
+            usernameInsta = extras.getString("usernameInsta");
+            pk = extras.getString("pk");
+            profile_pic_id = extras.getString("profile_pic_id");
+            no_of_followers = String.valueOf(extras.getInt("no_of_followers"));
+            followersUserNameList = extras.getStringArrayList("followersUserNameList");
+            followersPicUrlList = extras.getStringArrayList("followersPicUrlList");
+            Log.d(TAG, "my full_name from Extra is :" + full_name);
+            Log.d(TAG, "my profile_pic_url from Extra is :" + profile_pic_url);
+            Log.d(TAG, "my usernameInsta from Extra is :" + usernameInsta);
+            Log.d(TAG, "my pk from Extra is :" + pk);
+            Log.d(TAG, "my profile_pic_id from Extra is :" + profile_pic_id);
+            Log.d(TAG, "my no_of_followers from Extra is :" + no_of_followers);
+            Log.d(TAG, "my followersUserNameList from Extra is :" + followersUserNameList);
+            Log.d(TAG, "my followersPicUrlList from Extra is :" + followersPicUrlList);
+
+        } else {
+            return;
+        }
+
+        tvDisplay_name = findViewById(R.id.tvDisplay_name);
+        tvUserName = findViewById(R.id.tvUserName);
+        ivThumb = findViewById(R.id.ivThumb);
+        tvFolCount = findViewById(R.id.tvFolCount);
+
+        tvDisplay_name.setText(full_name);
+        tvUserName.setText(usernameInsta);
+        tvFolCount.setText(no_of_followers);
+        //tvPk.setText(pk);
+
+        Glide.with(this)
+                .load(profile_pic_url)
+                .into(ivThumb);
+
     }
+
+    public void onLogout(View view) {
+    }
+
 
 }
--- app/src/main/java/com/techline/buzzsocial/LoginActivity.java
+++ app/src/main/java/com/techline/buzzsocial/LoginActivity.java
@@ -5,21 +5,35 @@ import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.os.StrictMode;
+import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 
 import dev.niekirk.com.instagram4android.Instagram4Android;
+import dev.niekirk.com.instagram4android.requests.InstagramGetUserFollowersRequest;
+import dev.niekirk.com.instagram4android.requests.payload.InstagramGetUserFollowersResult;
+import dev.niekirk.com.instagram4android.requests.payload.InstagramLoggedUser;
+import dev.niekirk.com.instagram4android.requests.payload.InstagramLoginResult;
+import dev.niekirk.com.instagram4android.requests.payload.InstagramUserSummary;
 
 public class LoginActivity extends AppCompatActivity {
     public static final String MyPREFERENCES = "MyPrefs";
+    private static String TAG = "LOGIN";
 
     SharedPreferences SP;
     EditText pswd,usrusr;
     TextView sup,lin;
+    private long longPk = 0;
+    ArrayList<String> followersUserNameList = new ArrayList<String>();
+    ArrayList<String> followersPicUrlList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,29 +56,84 @@ public class LoginActivity extends AppCompatActivity {
         pswd.setTypeface(custom_font);
 
 
-
-
-        sup.setOnClickListener(new View.OnClickListener()
+        lin.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                Intent it = new Intent(LoginActivity.this, HomeActivity.class);
-                startActivity(it);
-                finish();
-            }
-        });
+                if (usrusr.getText().toString().trim().length() == 0) {
+                    Toast.makeText(getApplicationContext(), "Username is missing.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (pswd.getText().toString().trim().length() == 0) {
+                    Toast.makeText(getApplicationContext(), "Password is missing.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Instagram4Android instagram = Instagram4Android.builder().username(usrusr.getText().toString()).password(pswd.getText().toString()).build();
+                Log.d(TAG, "Before Setup");
+                instagram.setup();
+                Log.d(TAG, "after Setup");
+                try {
+                    Log.d(TAG, "Before login");
+                    InstagramLoginResult completeLoginResult = instagram.login();
+                    Log.d(TAG, "completeLoginResult >>"+completeLoginResult);
+                    String myMessage = completeLoginResult.getMessage();
+                    Log.d(TAG, "myMessage is " + myMessage);
+                    Toast.makeText(getApplicationContext(), myMessage, Toast.LENGTH_SHORT).show();
 
-    }
+                    String myStatus = completeLoginResult.getStatus();
+                    Log.d(TAG, "myStatus is " + myStatus);
+                    if (myStatus.equalsIgnoreCase("OK")) {
+                        Log.d(TAG, "myStatus is " + myStatus);
+                        Log.d(TAG, "status is oK run again");
+                        InstagramLoggedUser logged_in_user = completeLoginResult.getLogged_in_user();
+                        Log.d(TAG, "myStatus is " + myStatus);
+                        String AllUsrMsg = "You are Logged in as " + logged_in_user;
+                        Toast.makeText(getApplicationContext(), AllUsrMsg, Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, "AllUsrMsg is " + AllUsrMsg);
+                        String full_name = completeLoginResult.getLogged_in_user().full_name;
+                        Log.d(TAG, "full_name is " + full_name);
 
-    public void onClickButon(View view)  {
-        Instagram4Android instagram = Instagram4Android.builder().username(usrusr.getText().toString()).password(pswd.getText().toString()).build();
-        instagram.setup();
-        try {
-            instagram.login();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+                        String profile_pic_url = completeLoginResult.getLogged_in_user().profile_pic_url;
+                        Log.d(TAG, "profile_pic_url is " + profile_pic_url);
+                        String usernameInsta = completeLoginResult.getLogged_in_user().username;
+                        Log.d(TAG, "usernameInsta is " + usernameInsta);
+                        longPk = completeLoginResult.getLogged_in_user().pk;
+                        String pk = String.valueOf(completeLoginResult.getLogged_in_user().pk);
+                        Log.d(TAG, "pk is " + pk);
+                        String profile_pic_id = completeLoginResult.getLogged_in_user().profile_pic_id;
+                        Log.d(TAG, "profile_pic_id is " + profile_pic_id);
+
+                        InstagramGetUserFollowersResult allMyFollowers = instagram.sendRequest(new InstagramGetUserFollowersRequest(longPk));
+                        List<InstagramUserSummary> users = allMyFollowers.getUsers();
+                        for (InstagramUserSummary user : users) {
+                            Log.d(TAG, "User " + user.getUsername() + " follows "+full_name +"!");
+                            followersUserNameList.add(user.getUsername());
+                            Log.d(TAG, "user.getProfile_pic_url() " + user.getProfile_pic_url() + " follows "+full_name +"!");
+                            followersPicUrlList.add(user.getProfile_pic_url());
+                        }
+                        int no_of_followers = allMyFollowers.getUsers().size() + 1;
+                        Log.d(TAG, "no_of_followers >> "+no_of_followers);
+                        Intent it = new Intent(LoginActivity.this, HomeActivity.class);
+                        it.putExtra("full_name", full_name);
+                        it.putExtra("profile_pic_url", profile_pic_url);
+                        it.putExtra("usernameInsta", usernameInsta);
+                        it.putExtra("pk", pk);
+                        it.putExtra("profile_pic_id", profile_pic_id);
+                        it.putExtra("followersUserNameList", followersUserNameList);
+                        it.putExtra("no_of_followers", no_of_followers);
+                        it.putExtra("followersPicUrlList", followersPicUrlList);
+
+                        startActivity(it);
+                        finish();
+                    }
+                    else{
+                        return;
+                    }
+                    Log.d(TAG, "after login");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 /*        Instagram4Android instagram = new Instagram4Android(usrusr.getText().toString(),pswd.getText().toString());
         instagram.setup();
         try {
@@ -72,4 +141,14 @@ public class LoginActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }*/
+               /* Intent it = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(it);
+                finish();*/
+            }
+        });
+
+    }
+
+    public void onClickButon(View view)  {
+
     }}
